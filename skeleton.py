5a121d1c9bdaead7c02f474f9b52567af86767f7
Kristian Glass <git@doismellburning.co.uk>
2024-05-03 23:17:53 +0100

    Drop skeleton code

--- skeleton.py
+++
@@ -1,5 +0,0 @@
-import structlog
-
-log = structlog.get_logger()
-
-log.info("Hello, %s!", "world", key="value!", more_than_strings=[1, 2, 3])
--- test_sample.py
+++
@@ -1,6 +0,0 @@
-import skeleton
-
-
-def test_nothing():
-    _ = skeleton  # Dummy usage, just to persuade ruff we're using the import!
-    pass
